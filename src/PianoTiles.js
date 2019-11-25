50bd7d9ea25051cf4f6ccf51e02efa5c66f18b25
Albert Yee <albertyee84@gmail.com>
2019-11-25 15:07:43 -0800

    refactored code

--- src/PianoTiles.js
+++ src/PianoTiles.js
@@ -1,4 +1,3 @@
-
 const WIDTH = 100;
 const HEIGHT = 150;
 let tiles = [];
@@ -10,7 +9,7 @@ let lost;
 function setup() {
     createCanvas(401, 600);
     time = 3;
-    for (let i = 0; i < 4; i++){
+    for (let i = 0; i < 4; i++) {
         newRow();
     }
     score = 0;
@@ -25,47 +24,57 @@ function draw() {
     for (let i = 0; i < tiles.length; i++) {
         let x = (i % 4) * WIDTH + 1;
         let y = (Math.floor(i / 4) * HEIGHT);
-
-        fill((tiles[i] !== 0 ) ? ((tiles[i] === 1) ? "#FFFFFF" : "#FF0000") : "#000000" );
+        fill((tiles[i] !== 0) ? ((tiles[i] === 1) ? "#FFFFFF" : "#FF0000") : "#000000");
         rect(x, y, WIDTH, HEIGHT);
     }
     if (!playing) {
-        fill("#FF0000");
-        textSize(60);
-        text(time, width / 2, height / 2);
-
-        if (frameCount % 60 === 0) {
-            time--;
-            if (time === 0) {
-                playing = true;
-            }
-        }
+        countDown();
     }
     if (playing) {
-        time++;
-        fill("#FF0000");
-        textSize(60);
-        let t = Math.floor(time / 60) + ":" + time % 60;
-        text(t, width / 2, height  / 4);
-
+        timer();
     }
     if (lost) {
-        noLoop();
-        fill("#FF0000");
-        textSize(60);
-        text("Game Over!", width / 2, height / 2);
-        textSize(40);
-        text("Press f5 to restart!", width / 2, height / 2 + 50);
-        textSize(40);
-        text("Your score is: " + score, width / 2, height / 2 + 100);
+        lostMsg();
     }
 }
 
+function countDown() {
+    fill("#FF0000");
+    textSize(60);
+    text(time, width / 2, height / 2);
+
+    if (frameCount % 60 === 0) {
+        time--;
+        if (time === 0) {
+            playing = true;
+        }
+    }
+}
+
+function lostMsg() {
+    noLoop();
+    fill("#FF0000");
+    textSize(60);
+    text("Game Over!", width / 2, height / 2);
+    textSize(40);
+    text("Press esc to restart!", width / 2, height / 2 + 50);
+    textSize(40);
+    text("Your score is: " + score, width / 2, height / 2 + 100);
+}
+
+function timer() {
+    time++;
+    fill("#FF0000");
+    textSize(60);
+    let t = Math.floor(time / 60) + ":" + time % 60;
+    text(t, width / 2, height / 4);
+}
+
 function mousePressed() {
-    if(!playing) return;
-    if ( mouseY >= 3 * HEIGHT && mouseY <= 4* HEIGHT){
+    if (!playing) return;
+    if (mouseY >= 3 * HEIGHT && mouseY <= 4 * HEIGHT) {
         let t = getClickedTile(mouseX);
-        if (t === -1){
+        if (t === -1) {
             return;
         }
         if (tiles[t] !== 0) {
@@ -74,6 +83,7 @@ function mousePressed() {
         } else {
             score++;
             newRow();
+            popRows();
         }
     }
 }
@@ -81,30 +91,43 @@ function mousePressed() {
 function keyPressed() {
     if (!playing) return;
     let t;
-        switch(keyCode){
-            case 68:
-                t = 12;
-                break;
-            case 70:
-                t = 13;
-                break;
-            case 74:
-                t = 14;
-                break;
-            case 75:
-                t = 15;
-                break;
-            case 27:
-                setup();
-            default:
-                return;
-        }
+    let audio
+    switch (keyCode) {
+        case 68:
+            audio = document.getElementById(`c_octave1_audio`);
+            audio.play();
+            t = 12;
+            break;
+        case 70:
+            audio = document.getElementById(`f_octave1_audio`);
+            audio.play();
+            t = 13;
+            break;
+        case 74:
+            audio = document.getElementById(`a_octave1_audio`);
+            audio.play();
+            t = 14;
+            break;
+        case 75:
+
+            audio = document.getElementById(`e_octave1_audio`);
+            audio.play();
+            t = 15;
+            break;
+        case 27:
+            setup();
+            draw();
+            loop();
+        default:
+            return;
+    }
     if (tiles[t] !== 0) {
         tiles[t] = -1;
         endGame(false);
     } else {
         score++;
         newRow();
+        popRows();
     }
 }
 
@@ -114,12 +137,12 @@ function endGame(won) {
     } else {
         lost = true;
     }
-    
+
 }
 
 function getClickedTile(mouseX) {
-    for ( let i = 0; i < 4; i++) {
-        if( mouseX >= i * WIDTH && mouseX <= (i + 1) * WIDTH){
+    for (let i = 0; i < 4; i++) {
+        if (mouseX >= i * WIDTH && mouseX <= (i + 1) * WIDTH) {
             return i + 12;
         }
     }
@@ -129,6 +152,12 @@ function getClickedTile(mouseX) {
 function newRow() {
     let t = Math.floor(random(4));
     for (let i = 0; i < 4; i++) {
-        tiles.unshift(( i === t ) ? 0 : 1);
+        tiles.unshift((i === t) ? 0 : 1);
+    }
+}
+
+function popRows() {
+    for (let i = 0; i < 4; i++) {
+        tiles.pop();
     }
 }
---
+++ src/Tile.js
@@ -0,0 +1,8 @@
+class Tile {
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+}
+
+export default Tile;
